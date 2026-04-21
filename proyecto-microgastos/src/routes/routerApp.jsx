51ceecfbe9b29e97b4fc7c7ecf32a7418cc91203
Evelyn <evelinagudelosepulveda@gmail.com>
2026-04-21 17:54:06 -0500

    Terminando maquetacion de Registro y Login

--- proyecto-microgastos/src/routes/routerApp.jsx
+++ proyecto-microgastos/src/routes/routerApp.jsx
@@ -1,5 +1,5 @@
 import { createBrowserRouter } from "react-router-dom";
-import MainLayout from "../layouts/MainLayout";
+import MainLayout from "../components/Layout";
 import Login from "../pages/Login";
 import Register from "../pages/Register";
 import Dashboard from "../pages/Dashboard";
