3583ba9682e44f712ddec2b79477aa3e35bd5671
Evelyn <evelinagudelosepulveda@gmail.com>
2026-04-21 19:19:08 -0500

    Creacion del archivo New Expenses y mejoras en archivos de la carpeta de Components

--- proyecto-microgastos/src/routes/routerApp.jsx
+++ proyecto-microgastos/src/routes/routerApp.jsx
@@ -1,6 +1,5 @@
 import { createBrowserRouter } from "react-router-dom";
 import MainLayout from "../components/Layout";
-import Layout from "../components/Layout";
 import Login from "../pages/Login";
 import Register from "../pages/Register";
 import Dashboard from "../pages/Dashboard";
