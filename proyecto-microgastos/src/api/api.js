22c9d2b6af93ec65de4f9ce7cc7d892bae3d1b2e
Maria Fernanda <mafeospi0803@gmail.com>
2026-04-22 12:52:23 -0500

    corrigiendo api

--- proyecto-microgastos/src/api/api.js
+++ proyecto-microgastos/src/api/api.js
@@ -1,18 +1,16 @@
-// 1. Corregimos el puerto a 8080
-const BASE_URL = "http://localhost:8080"; 
+const BASE_URL = "http://localhost:8080/microgastosapp/v1";
 
 export const getGastos = async () => {
-  // 2. Usamos solo una vez /gastos
-  const res = await fetch(`${BASE_URL}/gastos`); 
+  const res = await fetch(`${BASE_URL}/gastos`);
   return res.json();
 };
 
 export const createGasto = async (gasto) => {
-  // 3. Quitamos el "gastos" extra para que no se duplique
-  const res = await fetch(`${BASE_URL}/gastos`, { 
+  const res = await fetch(`${BASE_URL}/gastos`, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify(gasto),
   });
+
   return res.json();
 };
