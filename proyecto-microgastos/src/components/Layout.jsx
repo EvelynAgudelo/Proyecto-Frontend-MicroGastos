d2733f5d21eb0608010919ea394bd0f7360da553
Evelyn <evelinagudelosepulveda@gmail.com>
2026-04-21 19:13:32 -0500

    Creacion del archivo NewExpenses y mejoras en archivos de la carpeta Components

--- proyecto-microgastos/src/components/Layout.jsx
+++ proyecto-microgastos/src/components/Layout.jsx
@@ -1,13 +1,17 @@
-import Navbar from "../components/Navbar";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
 import { Outlet } from "react-router-dom";
 
 function Layout() {
   return (
-    <div className="min-h-screen bg-gray-100">
+    <div className="min-h-screen flex flex-col">
       <Navbar />
-      <div className="p-6">
+
+      <div className="flex-grow p-6">
         <Outlet />
       </div>
+
+      <Footer />
     </div>
   );
 }
--- proyecto-microgastos/src/components/Navbar.jsx
+++ proyecto-microgastos/src/components/Navbar.jsx
@@ -1,13 +1,15 @@
+import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
-    <nav className="bg-gray-900 text-white px-6 py-4 flex justify-between items-center">
-      <h1 className="text-xl font-bold">💸 MicroGastos</h1>
+    <nav className="bg-gray-900 text-white px-6 py-4 flex justify-between">
+      <h1 className="font-bold">💸 MicroGastos</h1>
 
       <div className="space-x-4">
-        <Link to="/" className="hover:text-gray-300">Login</Link>
-        <Link to="/register" className="hover:text-gray-300">Registro</Link>
-        <Link to="/dashboard" className="hover:text-gray-300">Dashboard</Link>
+        <Link to="/">Login</Link>
+        <Link to="/register">Registro</Link>
+        <Link to="/dashboard">Dashboard</Link>
+        <Link to="/gasto">Nuevo Gasto</Link>
       </div>
     </nav>
   );
