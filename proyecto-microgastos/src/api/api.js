335c763c5dfb7bc78beeef7d5d212d2daf951848
Maria Fernanda <mafeospi0803@gmail.com>
2026-05-25 16:37:23 -0500

    Actualizando formulario de gastoService, Register, NewExpense y api

--- proyecto-microgastos/src/api/api.js
+++ proyecto-microgastos/src/api/api.js
@@ -1,16 +1,10 @@
-const BASE_URL = "http://localhost:8080/microgastosapp/v1";
+import axios from "axios";
 
-export const getGastos = async () => {
-  const res = await fetch(`${BASE_URL}/gastos`);
-  return res.json();
-};
+const api = axios.create({
+  baseURL: "http://localhost:8080/api",
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
 
-export const createGasto = async (gasto) => {
-  const res = await fetch(`${BASE_URL}/gastos`, {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(gasto),
-  });
-
-  return res.json();
-};
+export default api;
